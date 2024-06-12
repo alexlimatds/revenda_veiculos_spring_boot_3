74d1e6575a605546747feacc07ba08a6aa893328
Alexandre Lima <alexlimatds@yahoo.com.br>
2024-06-12 13:07:49 +0000

    Adição de página index.html

---
+++ src/main/java/net/revenda/controle/IndexController.java
@@ -0,0 +1,13 @@
+package net.revenda.controle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = {"/", "/index.html"})
+    public String index(){
+        return "index";
+    }
+}
