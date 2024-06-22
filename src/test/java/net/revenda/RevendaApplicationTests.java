2fe84964bb1143d441ffd7a90a0b22a9e11a5fad
Alexandre Lima <alexlimatds@yahoo.com.br>
2024-06-22 08:39:53 -0300

    Melhoria do teste em RevendaApplicationTests

--- src/test/java/net/revenda/RevendaApplicationTests.java
+++ src/test/java/net/revenda/RevendaApplicationTests.java
@@ -1,13 +1,17 @@
 package net.revenda;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class RevendaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertNotNull(context);
 	}
 
 }
